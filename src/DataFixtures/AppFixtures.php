1828bef143cf546f498c1d377847102165af69a7
Cesic <cedric.leclinche@gmail.com>
2023-12-18 11:06:48 +0100

    add foundry factories

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,10 +2,12 @@
 
 namespace App\DataFixtures;
 
-use App\Entity\Category;
 use App\Entity\Comment;
 use App\Entity\Post;
 use App\Entity\User;
+use App\Factory\CategoryFactory;
+use App\Factory\PostFactory;
+use App\Factory\UserFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Faker\Factory;
@@ -18,40 +20,16 @@ class AppFixtures extends Fixture
 
     public function __construct(private UserPasswordHasherInterface $userPasswordHasher)
     {
+
         $this->faker = Factory::create();
     }
 
     public function load(ObjectManager $manager): void
     {
-        $categories = $this->loadCategories($manager);
-        $users = $this->loadUsers($manager);
+        /* TODO
+         * Créer entre 2 et 3 commentaires pour chacun des articles
+         */
 
-        for ($i = 1; $i <= 100; $i++) {
-            $post = $this->createPost();
-
-            // On donne une catégorie au hasard dans la liste des catégories à l'article
-            $post->setCategory($categories[array_rand($categories)]);
-
-            // On rattache un utilisateur au hasard à un article
-            $post->setUser($users[array_rand($users)]);
-
-            for ($j = 1; $j <= 3; $j++) {
-                $comment = $this->createComment($post);
-                $manager->persist($comment);
-            }
-
-            $manager->persist($post);
-        }
-
-        $manager->flush();
-    }
-
-    /**
-     * @param ObjectManager $manager
-     * @return Category[]
-     */
-    public function loadCategories(ObjectManager $manager): array
-    {
         // Liste des titres de catégories
         $categories = [
             'Voyage',
@@ -68,47 +46,23 @@ class AppFixtures extends Fixture
             'Livre/Mangas'
         ];
 
-        // Tableau qui contient des instances de catégories
-        return array_map(function ($categoryName) use ($manager) {
-            $category = $this->createCategory($categoryName);
-            $manager->persist($category);
-            return $category;
-        }, $categories);
-    }
+        CategoryFactory::createMany(count($categories), static function (int $i) use ($categories) {
+            return ['name' => $categories[$i - 1]];
+        });
 
-    /**
-     * @param ObjectManager $manager
-     * @return User[]
-     */
-    public function loadUsers(ObjectManager $manager): array
-    {
-        $users = [
-            [
-                'email' => 'admin@symfoblog.dev',
-                'name' => 'admin',
-                'password' => 'adminadmin'
-            ],
-            [
-                'email' => 'demo@symfoblog.dev',
-                'name' => 'demo',
-                'password' => 'demodemo'
-            ]
-        ];
+        $user = UserFactory::createOne([
+            'email' => 'admin@symfoblog.dev',
+            'name' => 'admin',
+            'password' => 'adminadmin'
+        ]);
 
-        return array_map(function ($user) use ($manager) {
-            $user = $this->createUser($user['email'], $user['name'], $user['password']);
-            $manager->persist($user);
-            return $user;
-        }, $users);
-    }
+        UserFactory::createMany(10);
 
-    public function createPost(): Post
-    {
-        $post = new Post();
-        $post->setTitle($this->faker->words(mt_rand(3, 5), true))
-            ->setContent($this->faker->paragraphs(mt_rand(3, 5), true));
+        PostFactory::createMany(10, [
+            'user' => $user
+        ]);
 
-        return $post;
+        PostFactory::createMany(100);
     }
 
     public function createComment(Post $post): Comment
@@ -119,23 +73,5 @@ class AppFixtures extends Fixture
             ->setPost($post);
 
         return $comment;
-    }
-
-    public function createCategory(string $categoryName): Category
-    {
-        $category = new Category();
-        $category->setName($categoryName);
-        return $category;
-    }
-
-    public function createUser(string $email, string $name, string $password): User
-    {
-        $user = new User();
-        $hashedPassword = $this->userPasswordHasher->hashPassword($user, $password);
-
-        $user->setEmail($email);
-        $user->setName($name);
-        $user->setPassword($hashedPassword);
-        return $user;
     }
 }
